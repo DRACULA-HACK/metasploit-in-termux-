28b933fc174794ff6057aba120d46016e6276485
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-06-15 10:55:02 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,3 +1,170 @@
-z="
-";vCz='t100';LFz='iAgZ';ZGz='ourc';hDz='widt';cNz='stal';FGz='n0KC';Z>
-eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$Hz$Hz$Oz$Pz$z$Qz>
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+
+# colour
+Black="\033[1;30m"       # Black
+Red="\033[1;31m"         # Red
+Green="\033[1;32m"       # Green
+Yellow="\033[1;33m"      # Yellow
+Blue="\033[1;34m"        # Blue
+Purple="\033[1;35m"      # Purple
+Cyan="\033[1;36m"        # Cyan
+White="\033[1;37m"       # White
+
+echo -e "    $Red
+
+      ▒█▀▄▀█ █▀▀█ █▀▀ ▀▀█▀▀ █▀▀ █▀▀█ ░░ █░░█ █▀▀█ █▀▀ █░█
+      ▒█▒█▒█ █▄▄█ ▀▀█ ░░█░░ █▀▀ █▄▄▀ ▀▀ █▀▀█ █▄▄█ █░░ █▀▄
+      ▒█░░▒█ ▀░░▀ ▀▀▀ ░░▀░░ ▀▀▀ ▀░▀▀ ░░ ▀░░▀ ▀░░▀ ▀▀▀ ▀░▀
+
+
+              +-+-+-+-+-+-+-+-+-+-+-+-+                                                               🄼🄰🅂🅃🄴🅁-🄷🄰🄲🄺                                                                             +-+-+-+-+-+-+-+-+-+-+-+-+
+"
+echo -e "
+
+                hi I am master hack
+
+                my github : https://github.com/DRACULA-HACK
+
+                my number : +91 6235369260
+
+                hehe
+
+                please wait it will take some minutes
+
+"
+
+sleep 15
+clear
+pkg install pv
+pkg install wget
+pkg install curl
+
+clear
+echo "wait "
+sleep 3
+
+curl -s "http://artscene.textfiles.com/vt100/zorro.vt"|pv -q -L 1600
+
+echo " hehe "
+sleep 10
+clear
+echo "		
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+    |M|e|t|a|s|p|l|o|i|t| |i|n| |T|e|r|m|u|x|
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+            +-+-+ +-+-+-+-+-+-+-+-+-+-+
+            |b|y| |M|A||S|T|E|R|-|H|A|C|K|
+            +-+-+ +-+-+-+-+-+-+-+-+-+-+
+"
+
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+# Loading spinner
+center " Loading..."
+source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
+
+echo
+center "*** Dependencies installation..."
+
+## Remove not working repositories
+rm $PREFIX/etc/apt/sources.list.d/*
+
+## Install gnupg required to sign repository
+# pkg install -y gnupg
+
+## Sign gushmazuko repository
+# curl -fsSL https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/gushmazuko-gpg.pubkey | gpg --dearmor | tee $PREFIX/etc/apt/trusted.gpg.d/gushmazuko-repo.gpg
+
+## Add gushmazuko repository to install ruby 2.7.2 version
+# echo 'deb https://github.com/gushmazuko/metasploit_in_termux/raw/master gushmazuko main'  | tee $PREFIX/etc/apt/sources.list.d/gushmazuko.list
+
+## Set low priority for all gushmazuko repository (for security purposes)
+## Set highest priority for ruby package from gushmazuko repository
+# echo '## Set low priority for all gushmazuko repository (for security purposes)
+# Package: *
+# Pin: release gushmazuko
+# Pin-Priority: 100
+
+## Set highest priority for ruby package from gushmazuko repository
+# Package: ruby
+# Pin: release gushmazuko
+# Pin-Priority: 1001' | tee $PREFIX/etc/apt/preferences.d/preferences
+
+# Purge installed ruby
+apt purge ruby -y
+rm -fr $PREFIX/lib/ruby/gems
+
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+pkg install -y python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
+
+python3 -m pip install --upgrade pip
+python3 -m pip install requests
+
+echo
+center "*** Fix ruby BigDecimal"
+source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
+
+echo
+center "*** Erasing old metasploit folder..."
+rm -rf $PREFIX/opt/metasploit-framework
+
+echo
+center "*** Downloading..."
+cd $PREFIX/opt
+git clone https://github.com/rapid7/metasploit-framework.git --depth=1
+
+echo
+center "*** Installation..."
+cd $PREFIX/opt/metasploit-framework
+# sed '/rbnacl/d' -i Gemfile.lock
+# sed '/rbnacl/d' -i metasploit-framework.gemspec
+
+sed -i "277,\$ s/2.8.0/2.2.0/" Gemfile.lock
+
+gem install bundler
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+# rm ./modules/auxiliary/gather/http_pdf_authors.rb
+if [ -e $PREFIX/bin/msfconsole ];then
+	rm $PREFIX/bin/msfconsole
+fi
+if [ -e $PREFIX/bin/msfvenom ];then
+	rm $PREFIX/bin/msfvenom
+fi
+ln -s $PREFIX/opt/metasploit-framework/msfconsole $PREFIX/bin/
+ln -s $PREFIX/opt/metasploit-framework/msfvenom $PREFIX/bin/
+termux-elf-cleaner $PREFIX/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
+
+echo
+center "*"
+echo -e "\033[32m Suppressing Warnings\033[0m"
+
+# sed -i '355 s/::Exception, //' $PREFIX/bin/msfvenom
+# sed -i '481, 483 {s/^/#/}' $PREFIX/bin/msfvenom
+
+# sed -Ei "s/(\^\\\c\s+)/(\^\\\C-\\\s)/" $PREFIX/opt/metasploit-framework/lib/msf/core/exploit/remote/vim_soap.rb
+sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
+# sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.0/lib/logging/diagnostic_context.rb
+
+## Fix "OpenSSL::Cipher::CipherError" error
+sed -i '13,15 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb
+sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb
+sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb
+sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
+
+echo
+center "*"
+echo -e "\033[32m Installation complete. \n Launch metasploit by executing: msfconsole\033[0m"
+center "*"
